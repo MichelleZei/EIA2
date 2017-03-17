fbccc04f03441678648443f1f978df5ca22d077a
Sabine <Sabine@141.28.229.40>
2017-03-17 19:51:37 +0100

    Aufgabe0

---
+++ Aufgabe0/aufgabe0.js
@@ -0,0 +1,10 @@
+//Aufgabe: Aufgabe0 - Arbeisf�higkeit
+//Name: Michelle Zei�ner
+//Matrikel: 254665
+//Datum: 17.3.2017
+//Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+document.addEventListener('DOMContentLoaded', function () {
+    var input = prompt("Gib bitte deinen Namen ein:");
+    document.body.innerHTML = "Hallo " + input + "!";
+});
+//# sourceMappingURL=aufgabe0.js.map
---
+++ Aufgabe0/aufgabe0.ts
@@ -0,0 +1,10 @@
+//Aufgabe: Aufgabe0 - Arbeisf‰higkeit
+//Name: Michelle Zeiﬂner
+//Matrikel: 254665
+//Datum: 17.3.2017
+	
+//Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+document.addEventListener('DOMContentLoaded', function () {
+    var input: string = prompt("Gib bitte deinen Namen ein:");
+    document.body.innerHTML = "Hallo " + input + "!";
+});
