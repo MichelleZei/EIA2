8fd0bbbb68c61abf93853f6bf741ac739dc9d852
Sabine <Sabine@Sabine-NB.mshome.net>
2017-03-25 18:36:58 +0100

    aufgabe1

--- Aufgabe1/aufgabe1.js
+++ Aufgabe1/aufgabe1.js
@@ -3,47 +3,46 @@
 //Matrikel: 254665
 //Datum: 21.3.2017
 //Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
-
-document.addEventListener('DOMContentLoaded', function () {
-    var n = 5;
-    var c;
-    var x = 0;
-    var y = 0;
-    for (var i = 0; i < n; i++) {
-        y += (i == 2) ? 20 : 50;
-        x = (x + 170) % 400;
-        switch (i) {
-            case 0:
-                c = "#ff0000";
-                break;
-            case 1:
-            case 4:
-                c = "#00ff00";
-                break;
-            case 3:
-                continue;
-            default:
-                c = "#0000ff";
-        }
-        for (var a = 50; a > 0; a -= 20) {
-            placeDiv(c, x, y, a, a);
-            if (i == 4)
-                break;
-        }
+//
+var num = 5;
+for (let i = 0; i < num; i++) {
+    let y = 0;
+    let x = 0;
+    let c;
+    y += (i == 2) ? 20 : 50;
+    x = (x + 170) % 400;
+    switch (i) {
+        case 0:
+            c = "#ff0000";
+            break;
+        case 1:
+        case 4:
+            c = "#00ff00";
+            break;
+        case 3:
+            continue;
+        default:
+            c = "#0000ff";
     }
-    function placeDiv(_color, _x, _y, _width, _height) {
-        var div = document.createElement("div");
-        document.body.appendChild(div);
-        var s = div.style;
-        s.borderStyle = "solid";
-        s.borderColor = "#000000";
-        s.borderWidth = "1px";
-        s.position = "absolute";
-        s.display = "inline-block";
-        s.backgroundColor = _color;
-        s.width = _width + "px";
-        s.height = _height + "px";
-        s.left = _x + "px";
-        s.top = _y + "px";
+    for (let a = 50; a > 0; a -= 20) {
+        place(c, x, y, a);
+        if (i == 4)
+            break;
     }
-});
+}
+function place(c, x, y, a) {
+    let div = document.createElement("div");
+    document.body.appendChild(div);
+    let s = div.style;
+    s.borderStyle = "solid";
+    s.borderColor = "#000000";
+    s.borderWidth = "1px";
+    s.position = "absolute";
+    s.display = "inline-block";
+    s.backgroundColor = c;
+    s.width = a + "px";
+    s.height = a + "px";
+    s.left = x + "px";
+    s.top = y + "px";
+}
+//# sourceMappingURL=aufgabe1.js.map
